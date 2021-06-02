aafe70f821b57674d6510255976bfc689dcf38d8
vino <vino0908@outlook.com>
2021-06-02 14:30:37 +0800

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '/styles/globals.scss'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Head>
+                <title>next-starter</title>
+                <meta name="viewport" content="width=device-width,initial-scale=1"/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h1>hello Vino</h1>
+    );
+};
+
+export default Home;
