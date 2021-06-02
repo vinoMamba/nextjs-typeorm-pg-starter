a93d215c8de1732311c7b2c74bd004f1ce1b0103
vino <vino0908@outlook.com>
2021-06-02 14:45:28 +0800

    添加请求

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,21 @@
-const Home = () => {
+import {FC, useEffect, useState} from "react";
+import axios from "axios";
+
+
+const Home: FC = () => {
+    const [name, setName] = useState(null);
+    const [age, setAge] = useState(0);
+    useEffect(() => {
+        axios.get('/api/v1').then((response) => {
+            setAge(age => response.data.age);
+            setName(name => response.data.name);
+        });
+    }, []);
     return (
-        <h1>hello Vino</h1>
+        <>
+            <h1>hello Vino</h1>
+            <p>{name} -- {age}</p>
+        </>
     );
 };
 
